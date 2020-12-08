1102ea1e0593247bf997f80039b3a8b891ebb19e
mindscapeapp <mindscapegit@gmail.com>
2020-12-08 18:31:59 +0530

    First program in java

---
+++ first-java-project/src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+
+	// public - access specifier
+	// static - keyword
+	// void - return type of method (void means nothing)
+	// main - method name (Identifier)
+	public static void main(String[] args) {
+
+		// System - class in java
+		// out- object of PrintStream class
+		// println -method/function of PrintStream class
+		String s = "Hello World!";
+		System.out.println(s);
+		System.out.println(s);
+	}
+
+}
