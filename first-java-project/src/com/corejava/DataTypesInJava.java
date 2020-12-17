1835fa9158d73b958dd0fb8c127d68ef47054640
mindscapeapp <mindscapegit@gmail.com>
2020-12-17 18:05:02 +0530

    Data types in java

---
+++ first-java-project/src/com/corejava/DataTypesInJava.java
@@ -0,0 +1,29 @@
+package com.corejava;
+
+public class DataTypesInJava {
+	
+	public static void main(String[] args) {
+		
+		// below are primitive data types.
+		byte b=123;
+		System.out.println(b);
+		
+		short s=1234;
+		System.out.println(s);
+		
+		double f = 10.5;
+		System.out.println(f);
+		
+		boolean test=false;
+		System.out.println(test);
+		
+		// Below are non-primitive data types
+		
+		String str = "abc"; // predefined non-primitive data type
+		System.out.println(str);
+		
+		Student std = new Student(); // user defined non-primitive data type
+
+	}
+
+}
